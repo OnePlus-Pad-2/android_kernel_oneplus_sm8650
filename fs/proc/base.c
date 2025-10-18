229c9b1e8b64bb73fe714f38a185d680f4394cae
TheWildJames <bins4us@hotmail.com>
2025-10-18 15:41:29 -0400

    build: add susfs_def.h header to proc/base.c
    Include the susfs_def.h header file to support new functionality in the proc filesystem

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -100,6 +100,7 @@
 #include <linux/cn_proc.h>
 #include <linux/cpufreq_times.h>
 #include <linux/dma-buf.h>
+#include <linux/susfs_def.h>
 #include <trace/events/oom.h>
 #include <trace/hooks/sched.h>
 #include "internal.h"
